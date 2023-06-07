19238d7a9056129c4cdd5e44bcfc575c46f3d592
incisors <haj003@ucsd.edu>
2023-06-07 12:18:52 -0700

    server.js

--- reporting.cse135.monster/server.js
+++ reporting.cse135.monster/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+const cors = require('cors');
 
 app.use(express.static('public'));
+
+app.use(cors());
 
 app.listen(3005, function () {
   console.log('App listening on port 3005!');
 });
+
