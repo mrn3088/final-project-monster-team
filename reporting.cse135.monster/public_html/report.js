c06f9d0c7f9783af3d6d42350fb4a6bc16f4faae
incisors <haj003@ucsd.edu>
2023-06-07 12:10:30 -0700

    server.js

--- reporting.cse135.monster/public_html/report.js
+++ reporting.cse135.monster/public_html/report.js
@@ -429,5 +429,5 @@ app.delete('/activity/:userId', async (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://cse135.monster:${PORT}`);
 });
